0ef01c0f4344b4388b21d354226804290fa7a594
AliAgayevv <aghayev_233@mail.ru>
2024-10-30 01:29:05 +0400

    font and colors for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,20 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    extend: {
+      colors: {
+        custom: {
+          primary : "#0a1f44",
+          dark : "#1e2432",
+          darkRed: "#c3112d",
+          red : "#ff1e42",
+          orange: "#ff8c00"
+        }
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'], 
+      },
+      }
+    },
+    plugins: [],
+  }
